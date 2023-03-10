1e08753d0bdc2a60a33e1e70db6dc26bca5896db
cincip <126259768+cincip@users.noreply.github.com>
2023-03-10 21:12:23 +0600

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -24,3 +24,18 @@ class TestWin(QWidget):
         self.r_line = QVBoxLayout()
         self.h_line = QHBoxLayout()
         
+        self.text1 = QLabel(sdjnfksnfn)
+        self.text2 = QLabel(dsfgdgfsg)
+        self.button1 = QPushButton(dfgdsgsdg)
+        self.button2 = QPushButton(dfgdsgsdg)
+        self.button3 = QPushButton(dfgdsgsdg)
+        self.button4 = QPushButton(dfgdsgsdg)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.text1)
+        self.layout.addWidget(self.text2)
+        self.layout.addWidget(self.button1)
+        self.layout.addWidget(self.button2)
+        self.layout.addWidget(self.button3)
+        self.layout.addWidget(self.button4)
+        self.setLayout(self.layout)
