25d67b69711d8438e241b27922040e41918bd14c
dcmn1999 <127322117+dcmn1999@users.noreply.github.com>
2023-03-10 19:50:47 +0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,1 +1,21 @@
+from PyQt5.QtWidgets import QWidget
 
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()  # устанавливает, как будет выглядеть окно
+        self.initUI()  # создаём и настраиваем графические элементы
+        self.connects()  # устанавливает связи между элементами
+        self.show()  # старт
+
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+    def initUI(): 
+        pass
+    def connects(): 
+        pass
