32545f5d6db221775593a23f2632984a2bc1eaa7
SergeyKalutsky <skalutsky@gmail.com>
2023-03-10 17:28:50 +0300

    add MainWin class

--- my_app.py
+++ my_app.py
@@ -1,1 +1,9 @@
+from PyQt5.QtWidgets import QWidget
 
+class MainWin(QWidget):
+  def __init__(self):
+    super().__init__()
+    self.set_appear()# устанавливает, как будет выглядеть окно
+    self.initUI() # создаём и настраиваем графические элементы
+    self.connects() # устанавливает связи между элементами
+    self.show() # старт
